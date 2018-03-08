9097d6ea0843e00d7b35fdfc09763cf295358331
Nikolaj Lindberg <nikolaj@stts.se>
2018-03-08 13:09:33 +0100

    admin

---
+++ recserver/admin/admin.go
@@ -0,0 +1,72 @@
+package admin
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// TODO Return error? Doesn't check whether path is dir or file if it exists
+func userExists(audioDir, userName string) bool {
+	userDirName := filepath.Join(audioDir, strings.ToLower(userName))
+
+	fi, err := os.Stat(userDirName)
+	//if os.IsNotExist(err) {
+	//	return false
+	//} else {
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	if !fi.IsDir() {
+		return false
+	}
+
+	return true
+}
+
+func createBaseDir(d string) error {
+	_, err := os.Stat(d)
+	if !os.IsNotExist(err) {
+		return fmt.Errorf("dir already exists: '%s'", d)
+	}
+
+	err = os.MkdirAll(d, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to create base dir '%s' : ", d, err)
+	}
+
+	return nil
+}
+
+func subDirs(dirPath string) []os.FileInfo {
+	var res []os.FileInfo
+
+	fs, _ := ioutil.ReadDir(dirPath)
+	for _, f := range fs {
+		if f.IsDir() {
+			res = append(res, f)
+		}
+	}
+
+	return res
+}
+
+func addUser(baseDir, userName string) error {
+
+	userName = strings.ToLower(userName)
+	userDirName := filepath.Join(baseDir, userName)
+	_, err := os.Stat(userDirName)
+	if !os.IsNotExist(err) {
+		return fmt.Errorf("user already exists '%s'", userName)
+	}
+
+	err = os.MkdirAll(userDirName, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to add user '%s'", userName)
+	}
+
+	return nil
+}
---
+++ recserver/admin/admin_test.go
@@ -0,0 +1,43 @@
+package admin
+
+import (
+	"os"
+	"testing"
+)
+
+func TestUserExists(t *testing.T) {
+
+	d := "tmtTstUserDir"
+	_, err := os.Stat(d)
+	if !os.IsNotExist(err) {
+		os.RemoveAll(d)
+	}
+	err = createBaseDir(d)
+	if err != nil {
+		t.Errorf("failed to create dir '%s': %v", d, err)
+	}
+
+	u := "userZero"
+
+	ue1 := userExists(d, u)
+	if ue1 {
+		t.Errorf("expected false, got true")
+	}
+
+	err = addUser(d, u)
+	if err != nil {
+		t.Errorf("failed to add user '%s'", u)
+	}
+
+	ue2 := userExists(d, u)
+	if !ue2 {
+		t.Errorf("expected true, got false")
+	}
+
+	// Shouldn't be able to add same user twice
+	err = addUser(d, u)
+	if err == nil {
+		t.Errorf("should not be able to add existing user '%s'", u)
+	}
+
+}

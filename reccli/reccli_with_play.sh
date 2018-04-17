6cbf746737af429ffb3901162deb3956bef70843
Hanna Lindgren <han.lind@gmail.com>
2018-04-17 17:00:46 +0200

    reccli_with_play.sh

---
+++ reccli/reccli_with_play.sh
@@ -0,0 +1,14 @@
+cmd=$0
+dir=`dirname $cmd`
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $cmd <wavfiles>" 1>&2
+    exit 1
+fi
+
+cd $dir
+
+for wav in $*; do
+   play $wav &> /dev/null &
+   go run reccli.go -u tmptestxx $wav
+done

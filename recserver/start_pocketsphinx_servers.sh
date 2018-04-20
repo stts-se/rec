ead4f86b95365a0e8e9b38f70c771450c44d6bee
Hanna Lindgren <han.lind@gmail.com>
2018-04-20 10:40:45 +0200

    removed pocketsphinx script

--- recserver/start_pocketsphinx_servers.sh
+++
@@ -1,8 +0,0 @@
-git=`ls -d $HOME/git* | egrep "^.*/(git.*repos|git)$" | head -1`
-
-cd $git/e-lexia/pocketsphinx
-
-gunicorn -b localhost:8000 demo_server:api nst_10000_adapted_467 &
-gunicorn -b localhost:9090 demo_server:api nst_46912 &
-gunicorn -b localhost:9091 demo_server:api nst_46912_adapted_467 &
-gunicorn -b localhost:9999 demo_server:api elexia_448 &

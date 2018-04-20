0aadf2b0c2cca4587d3800d7b8277cd6388b6b7f
Hanna Lindgren <han.lind@gmail.com>
2018-04-20 15:19:49 +0200

    recserver: added debug message for incoming JSON

--- recserver/recserver.go
+++ recserver/recserver.go
@@ -104,12 +104,13 @@ func process0(w http.ResponseWriter, r *http.Request, verbMode bool) {
 		return
 	}
 
+	log.Printf("[recserver] incoming JSON string : %s\n", string(body))
+
 	input := rec.ProcessInput{}
 	err = json.Unmarshal(body, &input)
 	if err != nil {
 		msg := fmt.Sprintf("failed to unmarshal incoming JSON : %v", err)
 		log.Println(msg)
-		log.Printf("incoming JSON string : %s\n", string(body))
 		http.Error(w, msg, http.StatusBadRequest)
 		return
 	}
